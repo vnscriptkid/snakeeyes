1cdbb97796f484571fe10f9108ed5e86d166bd32
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-21 02:04:16 +0700

    Add contact blueprint and mailing task

--- snakeeyes/app.py
+++ snakeeyes/app.py
@@ -1,7 +1,37 @@
 from flask import Flask
+from celery import Celery
 
 from snakeeyes.blueprints.page import page
-from snakeeyes.extensions import debug_toolbar
+from snakeeyes.blueprints.contact import contact
+from snakeeyes.extensions import debug_toolbar, mail, csrf
+
+CELERY_TASK_LIST = [
+    'snakeeyes.blueprints.contact.tasks',
+]
+
+
+def create_celery_app(app=None):
+    """
+    Create a new Celery object and tie together the Celery config to the app's
+    config. Wrap all tasks in the context of the application.
+    :param app: Flask app
+    :return: Celery app
+    """
+    app = app or create_app()
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'],
+                    include=CELERY_TASK_LIST)
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
 
 
 def create_app(settings_override=None):
@@ -19,6 +49,7 @@ def create_app(settings_override=None):
         app.config.update(settings_override)
 
     app.register_blueprint(page)
+    app.register_blueprint(contact)
     extensions(app)
 
     return app
@@ -31,5 +62,7 @@ def extensions(app):
     :return: None
     """
     debug_toolbar.init_app(app)
+    mail.init_app(app)
+    csrf.init_app(app)
 
     return None
--- snakeeyes/extensions.py
+++ snakeeyes/extensions.py
@@ -1,4 +1,8 @@
+from flask_wtf import CsrfProtect
+from flask_mail import Mail
 from flask_debugtoolbar import DebugToolbarExtension
 
 
 debug_toolbar = DebugToolbarExtension()
+mail = Mail()
+csrf = CsrfProtect()
