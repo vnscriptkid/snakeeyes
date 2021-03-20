277b182297a87d9de4fb5a2c8efbd0a47429c41b
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-20 20:46:46 +0700

    Set up flask-debugtoolbar for development

--- 06-creating-a-base-flask-app/snakeeyes/app.py
+++ 06-creating-a-base-flask-app/snakeeyes/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
+
 from snakeeyes.blueprints.page import page
+from snakeeyes.extensions import debug_toolbar
 
 
 def create_app(settings_override=None):
@@ -17,5 +19,17 @@ def create_app(settings_override=None):
         app.config.update(settings_override)
 
     app.register_blueprint(page)
+    extensions(app)
 
     return app
+
+
+def extensions(app):
+    """
+    Register 0 or more extensions (mutates the app passed in).
+    :param app: Flask application instance
+    :return: None
+    """
+    debug_toolbar.init_app(app)
+
+    return None
