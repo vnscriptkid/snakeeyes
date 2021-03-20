e7d02e455c46cfc0d0886bddda39771c7b786553
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-20 16:24:16 +0700

    Set up conftest

--- 06-creating-a-base-flask-app/snakeeyes/app.py
+++ 06-creating-a-base-flask-app/snakeeyes/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from snakeeyes.blueprints.page import page
 
 
-def create_app():
+def create_app(settings_override=None):
     """
     Create a Flask application using the app factory pattern.
 
@@ -12,6 +12,9 @@ def create_app():
 
     app.config.from_object('config.settings')
     app.config.from_pyfile('settings.py', silent=True)
+
+    if settings_override:
+        app.config.update(settings_override)
 
     app.register_blueprint(page)
 
