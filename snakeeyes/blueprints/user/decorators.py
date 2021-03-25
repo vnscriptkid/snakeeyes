f1d630eebb5fbbdff75841a004b696fba644e534
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-26 00:34:38 +0700

    Add role_required decorator
    That checks if user has permission to view the page

--- snakeeyes/blueprints/user/decorators.py
+++ snakeeyes/blueprints/user/decorators.py
@@ -19,3 +19,23 @@ def anonymous_required(url='/settings'):
             return f(*args, **kwargs)
         return decorated_function
     return decorator
+
+
+def role_required(*roles):
+    """
+    Does a user have permission to view this page?
+    :param *roles: 1 or more allowed roles
+    :return: Function
+    """
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if current_user.role not in roles:
+                flash('You do not have permission to do that.', 'error')
+                return redirect('/')
+
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator
