3b0237a57a9033c63234a561f9a02f95b8b537c3
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-26 00:32:36 +0700

    Update utils

--- lib/util_wtforms.py
+++ lib/util_wtforms.py
@@ -16,3 +16,68 @@ class ModelForm(Form):
             self, obj=obj, prefix=prefix, **kwargs
         )
         self._obj = obj
+
+
+def choices_from_dict(source, prepend_blank=True):
+    """
+    Convert a dict to a format that's compatible with WTForm's choices. It also
+    optionally prepends a "Please select one..." value.
+
+    Example:
+      # Convert this data structure:
+      STATUS = OrderedDict([
+          ('unread', 'Unread'),
+          ('open', 'Open'),
+          ('contacted', 'Contacted'),
+          ('closed', 'Closed')
+      ])
+      # Into this:
+      choices = [('', 'Please select one...'), ('unread', 'Unread) ...]
+    :param source: Input source
+    :type source: dict
+    :param prepend_blank: An optional blank item
+    :type prepend_blank: bool
+    :return: list
+    """
+    choices = []
+
+    if prepend_blank:
+        choices.append(('', 'Please select one...'))
+
+    for key, value in source.items():
+        pair = (key, value)
+        choices.append(pair)
+
+    return choices
+
+
+def choices_from_list(source, prepend_blank=True):
+    """
+    Convert a list to a format that's compatible with WTForm's choices. It also
+    optionally prepends a "Please select one..." value.
+    Example:
+      # Convert this data structure:
+      TIMEZONES = (
+        'Africa/Abidjan',
+        'Africa/Accra',
+        'Africa/Addis_Ababa'
+      )
+      # Into this:
+      choices = [('', 'Please select one...'),
+                 ('Africa/Abidjan', 'Africa/Abidjan) ...]
+    :param source: Input source
+    :type source: list or tuple
+    :param prepend_blank: An optional blank item
+    :type prepend_blank: bool
+    :return: list
+    """
+    choices = []
+
+    if prepend_blank:
+        choices.append(('', 'Please select one...'))
+
+    for item in source:
+        pair = (item, item)
+        choices.append(pair)
+
+    return choices
